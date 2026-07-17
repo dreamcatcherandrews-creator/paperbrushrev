1026d7ba26c3de4e4e27291dfae803f596a5dc83
dreamcatcherandrews-creator <dreamcatcher.andrews@gmail.com>
2026-07-17 14:37:40 +0530

    Update email site.js

--- frontend/src/lib/site.js
+++ frontend/src/lib/site.js
@@ -10,7 +10,7 @@ export const SITE = {
     "https://customer-assets.emergentagent.com/job_minimal-portfolio-166/artifacts/0s3xlhqn_ChatGPT%20Image%20Jul%209%2C%202026%2C%2001_00_18%20PM.png",
   logoUnit:
     "https://customer-assets.emergentagent.com/job_minimal-portfolio-166/artifacts/kgapoc3x_P%26B%20logo%20unit.png",
-  email: "creator@paperbrushpictures.com",
+  email: "create@paperbrush.in",
   phone: "+91 98212 04202",
   phoneHref: "tel:+919821204202",
   address:
