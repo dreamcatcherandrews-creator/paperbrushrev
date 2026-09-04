16721b75b7bc82caaccf2ba151c066087d28cfba
dreamcatcherandrews-creator <dreamcatcher.andrews@gmail.com>
2026-09-04 20:56:11 +0530

    Fix video loading on Vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { FEATURED, FILMS } from "@/lib/films";
 import "@/App.css";
-import axios from "axios";
 import { Toaster } from "@/components/ui/sonner";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
@@ -11,30 +11,16 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import FilmDialog from "@/components/FilmDialog";
 
-const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
-
 function App() {
-  const [featured, setFeatured] = useState(null);
-  const [films, setFilms] = useState([]);
   const [active, setActive] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get(`${API}/films`)
-      .then((res) => {
-        setFeatured(res.data.featured);
-        setFilms(res.data.films || []);
-      })
-      .catch((e) => console.error("Failed to load films", e));
-  }, []);
 
   return (
     <div className="App pb-grain">
       <Navbar />
       <main>
         <Hero />
-        <FeaturedFilm film={featured} onPlay={setActive} />
-        <Showreel films={films} onPlay={setActive} />
+        <FeaturedFilm film={FEATURED} onPlay={setActive} />
+        <Showreel films={FILMS} onPlay={setActive} />
         <About />
         <Contact />
       </main>
